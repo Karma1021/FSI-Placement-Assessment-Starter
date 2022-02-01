ce4f6fb452a5e455eed6f848975138497d8ed6a1
django daponte <djangodaponte@gmail.com>
2022-01-31 21:33:42 -0800

    first commit

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
 // HINT: You can delete this console.log after you no longer need it!
 console.log('JavaScript code has loaded!')
 // First, tell us your name
-let yourName = "Jane Doe" // HINT: Replace this with your own name!
+let yourName = "Django Daponte" // HINT: Replace this with your own name!
 
 // We'll use these variables to track the counts of each cookie type
 let gb = 0 // Gingerbread
@@ -10,18 +10,41 @@ let sugar = 0 // Sugar Sprinkle
 
 // selecting the element with an id of credit
 const credit = document.querySelector('#credit')
-// selecting the element with an id of add-gb
-const gbPlusBtn = document.querySelector('#add-gb')
+
 
 // Code to update name display
 credit.textContent = `Created by ${yourName}`
 
-// Event listener for clicks on the "+" button for Gingerbread cookies
-gbPlusBtn.addEventListener('click', function() {
+let totalgb = 0
+let gbPlusBtn = document.getElementById('add-gb')
+let qtygb= document.getElementById('qty-gb')
+let gbMinusBtn = document.getElementById('minus-gb')
+
+
+gbPlusBtn.addEventListener('click', function(i){
+gb++
+qtygb.textContent = gb
+
+gbMinusBtn.addEventListener('click', function(i){
+    gb = gb -1
+    qtygb.textContent = gb
+    
+
+
+})
+
+
+
+
+
+
+
+
+
 // HINT: You can delete this console.log after you no longer need it!
-console.log('Gingerbread + button was clicked!')
+
 
 // TODO: Write the code to be run when the "+" button for "Gingerbread" is clicked
-})
+
 
 // TODO: Hook up event listeners for the rest of the buttons
